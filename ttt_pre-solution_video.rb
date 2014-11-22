8fa0b30ee0a1030b785da63e441c41b78dc299e7
Kyle Turner <turnerdkyle@icloud.com>
2014-11-21 21:15:11 -0700

    Start of code for tic tac toe game

--- ttt_pre-solution_video.rb
+++ ttt_pre-solution_video.rb
@@ -1,36 +1,35 @@
 # The gameboard to be drawn to the console
-
 def draw_gameboard
 
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                  1|                  2|                    3|'
-puts '            ---------------------------------------------------------------'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                  4|                  5|                    6|'
-puts '            ---------------------------------------------------------------'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                   |                   |                     |'
-puts '            |                  7|                  8|                    9|'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                  1|                  2|                    3|'
+puts '                  ---------------------------------------------------------------'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                  4|                  5|                    6|'
+puts '                  ---------------------------------------------------------------'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                   |                   |                     |'
+puts '                  |                  7|                  8|                    9|'
 
 end
 
@@ -50,12 +49,36 @@ winning_square_combinations = [[1, 2, 3],
 puts 'Hello! Ready to play some Tic-Tac-Toe?'
 puts 'Y/N'
 user_play_decision = gets.chomp
+user_square_selection = ''
 
 # Conditional depends on user input
 if user_play_decision.upcase == 'Y'
-puts '                  <CHOOSE A BLOCK BY TYPING IT\'S CORRESPONDING NUMBER>'
+puts '                        <CHOOSE A BLOCK BY TYPING IT\'S CORRESPONDING NUMBER>'
   draw_gameboard
+
 else
   exit
 end
 
+case user_square_selection
+  when user_square_selection == gameboard_square_numbers[0]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[1]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[2]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[3]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[4]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[5]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[6]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[7]
+    # puts something
+  when user_square_selection == gameboard_square_numbers[8]
+    # puts something
+  else
+    puts 'Please choose a number between 1 and 9'
+end
