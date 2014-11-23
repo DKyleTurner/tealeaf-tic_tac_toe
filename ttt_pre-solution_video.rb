08000a83bfb30619ff19ec15b9d025d8c0f2bffc
Kyle Turner <turnerdkyle@icloud.com>
2014-11-22 19:59:58 -0700

    Still trying to rework the logic

--- ttt_pre-solution_video.rb
+++ ttt_pre-solution_video.rb
@@ -1,87 +1,59 @@
-# The gameboard to be drawn to the console
-def draw_gameboard
-
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                  1|                  2|                    3|'
-puts '                  ---------------------------------------------------------------'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                  4|                  5|                    6|'
-puts '                  ---------------------------------------------------------------'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                   |                   |                     |'
-puts '                  |                  7|                  8|                    9|'
-
-end
-
-gameboard_square_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# Array that stores all the possible winning square combinations
-winning_square_combinations = [[1, 2, 3],
-                               [4, 5, 6],
-                               [7, 8, 9],
-                               [1, 4, 7],
-                               [2, 5, 8],
-                               [3, 6, 9],
-                               [1, 5, 9],
-                               [7, 5, 3]]
-
 # Greeting message asks user whether they want to play or not
 puts 'Hello! Ready to play some Tic-Tac-Toe?'
 puts 'Y/N'
+square_selections = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
 user_play_decision = gets.chomp
-user_square_selection = ''
 
-# Conditional depends on user input
-if user_play_decision.upcase == 'Y'
-puts '                        <CHOOSE A BLOCK BY TYPING IT\'S CORRESPONDING NUMBER>'
-  draw_gameboard
+while user_play_decision.upcase == 'Y'
 
-else
-  exit
+
+  # The gameboard to be drawn on the console
+  first_row_spaces = "                  |     #{square_selections[0]}     |      #{square_selections[1]}     |      #{square_selections[2]}     |"
+  second_row_spaces = "                  |     #{square_selections[3]}     |      #{square_selections[4]}     |      #{square_selections[5]}     |"
+  third_row_spaces = "                  |     #{square_selections[6]}     |     #{square_selections[7]}      |     #{square_selections[8]}      |"
+
+  puts '           <CHOOSE A SQUARE BY TYPING IT\'S CORRESPONDING NUMBER>'
+
+  puts '                  ^           ^            ^            ^'
+  puts '                  |           |            |            |'
+  puts '                  |           |            |            |'
+  puts first_row_spaces
+  puts '                  |           |            |            |'
+  puts '                  |         (1|          (2|          (3|'
+  puts '               <--+-----------+------------+------------+-->'
+  puts '                  |           |            |            |'
+  puts '                  |           |            |            |'
+  puts second_row_spaces
+  puts '                  |           |            |            |'
+  puts '                  |         (4|          (5|          (6|'
+  puts '               <--+-----------+------------+------------+-->'
+  puts '                  |           |            |            |'
+  puts '                  |           |            |            |'
+  puts third_row_spaces
+  puts '                  |           |            |            |'
+  puts '                  |         (7|          (8|          (9|'
+  puts '                  v           v            v            v'
+
+
+
+  if user_play_decision.upcase == 'Y'
+  else
+    exit
+  end
+
+
+  puts 'What do you say?'
+  user_square_choice = gets.chomp
+
+  if user_square_choice == '5' # This part successfully prints 'X'...
+    square_selections[4] = 'X'
+    system('clear')
+
+  elsif user_square_choice == '6'
+    square_selections[5] == 'X' # This part never gets run because we have already initialized the var
+    # system('clear')
+  end
+
 end
-
-########## NEED TO ALLOW USER TO CHOOSE A SQUARE TO PUT THEIR 'X' OR 'O' ################
-
-########## NEED TO DISPLAY AN 'X' OR 'O' FOR COMPUTER'S TURN ###################
-
-# Need to figure out a way to get an 'X' or an 'O' onto the board when the user chooses a square
-user_square_display = case user_square_selection
-                      when gameboard_square_numbers[0] then
-                      when gameboard_square_numbers[1] then
-                      when gameboard_square_numbers[2] then
-                      when gameboard_square_numbers[3] then
-                      when gameboard_square_numbers[4] then
-                      when gameboard_square_numbers[5] then
-                      when gameboard_square_numbers[6] then
-                      when gameboard_square_numbers[7] then
-                      when gameboard_square_numbers[8] then
-                      else puts 'Please choose a number between 1 and 9'
-                      end
-
-############ NEED TO CHECK AND SEE IF THE USER OR COMPUTER HAS WON, LOST OR TIED ################
-
-############ IF A 'WIN, LOSS OR TIE' CONDITION IS MET... ####################
-
-############ ASK IF THE USER WOULD LIKE TO PLAY AGAIN #######################
-
-############ IF YES, PLAY AGAIN, IF NO, END GAME ############################
+# system('clear') - Automatically clears the console
